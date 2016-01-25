cc9a6a5876e717f157d4a3c179888d2fef743034
enterth3r4in <believe.the.rain@gmail.com>
2016-01-25 13:09:18 -0500

    Homework Update
    Completed homework

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Homework 1
+ * Author: Cody Miller
+ * Project Purpose: Program to Display Hello World
+ * Input:  This program does not accept inputs
+ * Desired Output:  The message Hello World is displayed in the console
+ * Variables and Classes:  HelloWorld
+ * Formulas:  Main Method only
+ * Description of the main algorithm: system.out.println is used to display the output
+
+ * Testing:  Run program
+ * January 16, 2016
+ */
+
+
 namespace HelloWorld
 {
-    /**
-     * Author: Cody Miller
-     * 
-     **/
     class HelloWorld
     {
         static void Main(string[] args)
